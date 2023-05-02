03523c1a436daf41b26643ed6aa743da35145409
sumit <118124900+sumit-yu-bot@users.noreply.github.com>
2023-05-02 17:18:34 +0530

    Update Soundcloud.py

--- kannadiga/platforms/Soundcloud.py
+++ kannadiga/platforms/Soundcloud.py
@@ -3,7 +3,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from ShizukaXMusic.utils.formatters import seconds_to_min
+from kannadiga.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
